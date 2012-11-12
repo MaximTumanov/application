7ba959fd348cb21efac822a36e648c1819321d70
a-strug <a.strug.ua@gmail.com>
2012-11-12 21:13:46 +0200

    fuckin encoding

--- views/templates/default.php
+++ views/templates/default.php
@@ -71,7 +71,7 @@
 	<?php foreach($scripts as $file) { echo HTML::script($file, null, TRUE); }?>
 	</noindex>
 
-	<div id="top"><nobr>Ð?Ð°Ð²ÐµÑ€Ñ… &uarr;</nobr></div>
+	<div id="top"><nobr>Вверх… &uarr;</nobr></div>
 
 <!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter17922640 = new Ya.Metrika({id:17922640, enableAll: true, trackHash:true, webvisor:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/17922640" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
 	
