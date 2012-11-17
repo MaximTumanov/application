ed1adf73b4be1a910de72b38c62f29f850f3ff41
a-strug <a.strug.ua@gmail.com>
2012-11-17 16:56:58 +0200

    free event

--- views/pages/blog_all.php
+++ views/pages/blog_all.php
@@ -9,13 +9,8 @@
 					$href = Route::url('blog', array('controller' => 'blog', 'action' => 'show', 'item_alias' => $blog->alias));
 					$month = $anons_config['month'][$month];
 					HTML::replaceImg(&$blog->desc);
-/*tidy_setopt('output-xml' , true ); 
-tidy_setopt('input-encoding' , 3 ); 
-tidy_setopt('output-encoding' , 3 ); 
-tidy_setopt('language' , 'ru' );*/
-
 					$desc = tidy_repair_string(HTML::cropstr($blog->desc, 200), array(), 'utf8');
-					$has_href = ($blog->images_folder || substr($desc, -3) == '...') ? true : false ;
+					$has_href = true;#($blog->images_folder || substr($desc, -3) == '...') ? true : false ;
 				?>
 
 				<div itemscope itemtype="http://schema.org/Event">
@@ -35,23 +30,6 @@ tidy_setopt('language' , 'ru' );*/
 					</div>
 				</div>
 
-				<?/*
-				<div class="p_href" href="<?php echo $href;?>" alt="<?php echo $blog->title?>">
-					<div class="place_line">
-						<div class="p_img"><?php echo HTML::MegaImg('blog', 'no_img_events.gif', 'post_load', '', $blog->title, $anons_config['EVENT_IMG_ALL'], $blog->image);?></div>
-						<div class="p_info">
-              <p class="date"><span><?php echo $day?></span> <?php echo $month?></p>
-							<h2><?php echo $blog->title?></h2>
-
-							<div class="ghj">
-								<?php echo HTML::cropstr($blog->s_desc, 25)?>
-              </div>
-						</div>
-
-						<div class="clearer"></div>
-					</div>
-				</div>
-				*/?>
 			<?php endforeach;?>
 		</div>
 		<?php if (count($blogList) > 30):?>
