c78ad32c62f6848ce6cc2c6bd09383646772e02b
a-strug <a.strug.ua@gmail.com>
2012-11-17 15:56:38 +0200

    .

--- views/pages/places_archive.php
+++ views/pages/places_archive.php
@@ -4,7 +4,7 @@
 <div class="left upcoming">
 	<div class="wrapp">
 		<h1 class="color-orang ttl"><a href="<?php echo $placeHref?>"><?php echo $title?></a></h1>
-		<p class="center"><span>в архиве</span></p>
+		<p class="center" style="text-align: center; margin-top: -15px"><span>в архиве</span></p>
 
 		<div id="places_list">
 			<div class="filtering_box">
