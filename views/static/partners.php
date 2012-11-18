359fb31cccdf38521bcdb526b34d5f225aa5f0dc
a-strug <a.strug.ua@gmail.com>
2012-11-18 15:26:40 +0200

    partners

--- views/static/partners.php
+++ views/static/partners.php
@@ -1,11 +1,19 @@
+<?php
+	$model = new Model_Place();
+	$partner_ids = "263, 156, 299, 346";
+	$q = "SELECT * FROM `jos_places` WHERE id_place IN({$partner_ids})";
+	$partnerList = DB::query(Database::SELECT, $q)->as_object()->execute()->as_array();
+?>
 <div class="left upcoming item">
 	<div class="wrapp">
 		<div class="">
 
 			<div id="partners_list">
-				<?php for($i = 0; $i < 12; $i++):?>
-				<div data-info='{"latlng":[48.46442771287488, 35.043768882751465]}' class="partner"><img src="/images/logo.jpg" /></div>
-				<?php endfor;?>
+				<?php foreach($partnerList as $key => $partner):
+					list($x, $y, $z) = explode(":", $partner->google);
+				?>
+				<div data-rel="<?php echo $key?>" class="partner"><img title="<?php echo addslashes($partner->title)?>" src="/images/partners/partner-<?php echo $partner->id_place?>.jpg" /></div>
+				<?php endforeach;?>
 				<div class="clearer"></div>
 			</div>
 
@@ -17,6 +25,10 @@
 
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
 <script type="text/javascript">
+var arrMarkers = []
+	, arrInfoWindows = []
+	, map;
+
 var createMap = function () {
 
 	var latlng = new google.maps.LatLng(48.46442771287488, 35.043768882751465);
@@ -27,22 +39,48 @@ var createMap = function () {
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};	
 
-	var map = new google.maps.Map(document.getElementById("google_maps_partner"), myOptions);	
+	map = new google.maps.Map(document.getElementById("google_maps_partner"), myOptions);	
+
+	<?php foreach($partnerList as $key => $partner):
+		list($x, $y, $z) = explode(":", $partner->google);
+	?>
 
 	var marker = new google.maps.Marker({
-	    position: latlng, 
+	    position: new google.maps.LatLng(<?php echo $y?>, <?php echo $x?>), 
 	    map: map,
-	    title:""
+	    title:"<?php echo addslashes($partner->title)?>"
 	});
 
+	var desc = "<p style='text-align: center'><img src='/images/partners/partner-<?php echo $partner->id_place?>.jpg' /></p>";
+			desc += "<p><b>Адрес</b>: <?php echo $partner->address?></p>";
+
+			<?php if($partner->tel):?>
+				desc += "<p><b>Телефон</b>: <?php echo $partner->tel?></p>";
+			<?php endif;?>
+
+			<?php if($partner->email):?>
+				desc += "<p><b>Email</b>: <?php echo $partner->email?></p>";
+			<?php endif;?>
+
+	attachInfoWindow(map, marker, desc, <?php echo $key?>);
+
+	<?php endforeach;?>
+}
+
+function attachInfoWindow(map, marker, desc, key){
 	var infowindow = new google.maps.InfoWindow({ 
-		content: '',
-		size: new google.maps.Size(50, 50),
-		position: ''
+		content: desc,
+		size: new google.maps.Size(50, 50)
 	});
 
 	google.maps.event.addListener(marker, 'click', function() {
 	    infowindow.open(map, marker);
-	});
+	});	
+
+	arrMarkers[key] = marker;
+	arrInfoWindows[key] = infowindow;
 }
+
+createMap();
+
 </script>
