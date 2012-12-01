45564d2706b765ee6f5a6c584df1ab53454ace54
a-strug <a.strug.ua@gmail.com>
2012-12-01 14:44:59 +0200

    .

--- views/widgets/search.php
+++ views/widgets/search.php
@@ -7,7 +7,7 @@
 </script>
 
 
-<div style="margin-bottom: 25px;">
+<div style="margin-top: 25px;">
 	<div id="vk_groups"></div>
 	<script type="text/javascript">
 		VK.Widgets.Group("vk_groups", {mode: 0, width: "330", height: "290"}, 42894327);
