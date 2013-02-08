fff9859bb675004dfd36f9a2d459f4d79c5e60e6
a-strug <a.strug.ua@gmail.com>
2013-02-08 19:34:40 +0200

    .

--- views/pages/blog.php
+++ views/pages/blog.php
@@ -31,7 +31,11 @@
       <div class="blog_text">
         <?php echo $item->desc?>              
       </div>
-   		
+
+      <?php if ($item->vk_video):?>
+        <iframe type="text/html" width="607" height="360" src="<?php echo $item->vk_video; ?>" frameborder="0"></iframe>
+      <?php endif;?>
+
       <?php if(isset($use_vkontakte_comment)):?>
           <script type="text/javascript">
             VK.init({apiId: 3192678, onlyWidgets: true});
